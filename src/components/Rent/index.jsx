baba063b130af04e58b389395897a9c4eb6a5acc
hasanakpolad <hsnakpld@gmail.com>
2022-07-01 12:51:31 +0300

    deploy

--- src/components/Rent/index.jsx
+++ src/components/Rent/index.jsx
@@ -15,7 +15,7 @@ function Rent() {
                     <h1 className="text-uppercase tm-banner-title">Let's begin</h1>
                     <img src={require("./images/dots-3.png")} alt="Dots" />
                     <p className="tm-banner-subtitle">We assist you to choose the best.</p>
-                    <a href="#" className="tm-down-arrow-link"><i className="fa fa-2x fa-angle-down tm-down-arrow"></i></a>
+                    {/* <a href="#" className="tm-down-arrow-link"><i className="fa fa-2x fa-angle-down tm-down-arrow"></i></a> */}
                   </div>
                 </div>
                 {/* <!-- col-xs-12 --> */}
@@ -110,7 +110,7 @@ function Rent() {
             <div className="row">
               <div className="col-xs-12 mx-auto tm-about-text-wrap text-center">
                 <h2 className="text-uppercase mb-4">Your <strong>Journey</strong> is our priority</h2>
-                <p className="mb-4">Nullam auctor, sapien sit amet lacinia euismod, lorem magna lobortis massa, in tincidunt mi metus quis lectus. Duis nec lobortis velit. Vivamus id magna vulputate, tempor ante eget, tempus augue. Maecenas ultricies neque magna.</p>
+                <p className="mb-4" style={{color:"black"}}>Nullam auctor, sapien sit amet lacinia euismod, lorem magna lobortis massa, in tincidunt mi metus quis lectus. Duis nec lobortis velit. Vivamus id magna vulputate, tempor ante eget, tempus augue. Maecenas ultricies neque magna.</p>
                 <a href="#" className="text-uppercase btn-primary tm-btn">Continue explore</a>
               </div>
             </div>
